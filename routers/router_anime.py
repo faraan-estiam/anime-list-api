41c911d906975e312472d915cedcf7baeeaee232
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-10-12 00:06:19 +0200

    added get anime by id

--- routers/router_anime.py
+++ routers/router_anime.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from classes.models import Anime
 from classes.database import animes
 
@@ -11,3 +11,11 @@ router = APIRouter(
 @router.get('', response_model=list[Anime])
 async def get_animes():
     return animes
+
+#get specific anime by uid
+@router.get('/{anime_uid}', response_model=Anime)
+async def get_anime_by_id(anime_uid:str):
+    for anime in animes:
+        if str(anime.uid)==anime_uid:
+            return anime
+    raise HTTPException(status_code=404, detail='anime not found')
