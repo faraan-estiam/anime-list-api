de520e31b7e981527c6a6d566985f61e995fc703
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-10-18 22:47:18 +0200

    import fix

--- database/firebase.py
+++ database/firebase.py
@@ -1,6 +1,6 @@
 import firebase_admin, pyrebase
 from firebase_admin import credentials
-from etc.secrets.firebase_config import firebaseConfig
+from firebase_config import firebaseConfig
 
 cred = credentials.Certificate('etc/secrets/anime-list-api-private-key.json')
 firebase_admin.initialize_app(cred)
