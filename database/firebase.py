4983f39fb23429d503c9b9391c8d66b87ba20142
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-10-18 23:17:59 +0200

    updated secret path

--- database/firebase.py
+++ database/firebase.py
@@ -1,8 +1,8 @@
 import firebase_admin, pyrebase
 from firebase_admin import credentials
-from configs.firebase_config import firebaseConfig
+from firebase_config import firebaseConfig
 
-cred = credentials.Certificate('configs/anime-list-api-private-key.json')
+cred = credentials.Certificate('anime-list-api-private-key.json')
 firebase_admin.initialize_app(cred)
 
 firebase=pyrebase.initialize_app(firebaseConfig)
